e076c34514f826519296223b6bf14bee86b88dcf
suryanshaggarwal <suryan0@students.ubc.ca>
2025-09-01 10:38:27 -0700

    added express.JSON

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 
+
+app.use(express.json())
 app.use(express.static('dist'))
 
 morgan.token('json-content', (req, res) => {
@@ -21,39 +23,7 @@ app.use(morgan(function (tokens, req, res) {
 
 
 
-let names = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
-
-
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json'})
-//   response.end(JSON.stringify(names))
-// })
-
-// const PORT = 3001
-// app.listen(PORT)
-// console.log(`Server running on port ${PORT}`)
 app.get('/api/persons', (request, response) => {
   response.json(names)
 })
